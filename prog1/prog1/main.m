25da3d6bccf7218076dd73504136eb25fe206dce
woonohyo <wonhyo.yi@gmail.com>
2014-01-12 18:08:00 +0900

    prog 2.1
    using command line toop

---
+++ prog1/prog1/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Wonhyo Yi on 2014. 1. 12..
+//  Copyright (c) 2014년 NHN NEXT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"iOS Programming is Fun!");
+    }
+    return 0;
+}
+
