865d8016cb244fb9ddbac1b9250b9e16a49bfc35
LinePlus <lineplus@LinePlus-ui-MacBook-Pro.local>
2014-01-13 17:47:31 +0900

    Initial Commit

---
+++ ObjectiveC2.0/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjectiveC2.0
+//
+//  Created by LinePlus on 2014. 1. 13..
+//  Copyright (c) 2014년 LinePlus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
