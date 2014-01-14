f061da62698c12b94471c6c044a55b4ca5a21831
LinePlus <lineplus@LinePlus-ui-MacBook-Pro.local>
2014-01-14 17:43:49 +0900

    Number Objectives

--- ObjectiveC2.0/main.m
+++ ObjectiveC2.0/main.m
@@ -12,10 +12,41 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        NSNumber    *myNumber, *floatNumber, *intNumber;
+        NSInteger   myInt;
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //Integer value
+        intNumber = [NSNumber numberWithInteger:100];
+        myInt = [intNumber integerValue];
+        NSLog(@"%li", (long) myInt);
         
+        //Long value
+        myNumber = [NSNumber numberWithLong: 0xbab0bab0];
+        NSLog(@"%lx", [myNumber longValue]);
+        
+        //Char value
+        myNumber = [NSNumber numberWithChar:'X'];
+        NSLog(@"%c", [myNumber charValue]);
+        
+        //float value
+        floatNumber = [NSNumber numberWithFloat:100.00];
+        NSLog(@"%g", [floatNumber floatValue]);
+        
+        //double value
+        myNumber = [NSNumber numberWithDouble:12345e+15];
+        NSLog(@"%lg", [myNumber doubleValue]);
+        
+        //Error
+        NSLog(@"%li", (long) [myNumber integerValue]);
+        
+        //equal or not equal
+        if ([intNumber isEqualToNumber:floatNumber] == YES)
+            NSLog(@"Numbers are equal");
+        else NSLog(@"Nubmers are not equal");
+        
+        //larger or smaller
+        if ([intNumber compare: myNumber] == NSOrderedAscending)
+            NSLog(@"First number is less than second");
     }
     return 0;
 }
