669d881b03c99632ab5fc018aca18568f693f361
woonohyo <wonhyo.yi@gmail.com>
2014-01-14 21:36:20 +0900

    prog 15.9 address card using property and synthesize.

--- ObjectiveC2.0/AddressCard.h
+++ ObjectiveC2.0/AddressCard.h
@@ -10,11 +10,11 @@
 
 @interface AddressCard : NSObject
 
-- (void) setName: (NSString *) theName;
-- (void) setEmail: (NSString *) theEmail;
-- (NSString *) name;
-- (NSString *) email;
-
+// copy = 세터 메서드에서 인스턴스 변수의 사본을 만들라는 의미.
+// (기본 속성은 단순하게 대입하는 assign. 부적절함)
+// nonatomic = 다중 스레드에서 동시에 한 인스턴스에 접근하려 할 때
+// 발생할 수 있는 경쟁조건에 대해 걱정할 필요가 없다고 지시.
+@property (copy, nonatomic) NSString *name, *email;
 - (void) print;
 
 @end
--- ObjectiveC2.0/AddressCard.m
+++ ObjectiveC2.0/AddressCard.m
@@ -9,36 +9,11 @@
 #import "AddressCard.h"
 
 @implementation AddressCard
-{
-    NSString *name;
-    NSString *email;
-}
+@synthesize name, email;
 
 
 // name = theName 으로 하면 AddressCard 객체가 자신의 멤버 객체를 소유하지 않게 된다.
 // 메서드로 건네지는 인수의 참조만 담고 있게 됨.
-- (void) setName:(NSString *)theName
-{
-    if (name != theName)
-        name = [NSString stringWithString:theName];
-}
-
-- (void) setEmail:(NSString *)theEmail
-{
-    if (email != theEmail)
-        email = [NSString stringWithString:theEmail];
-}
-
-- (NSString *)name
-{
-    return name;
-}
-
-- (NSString *)email
-{
-    return email;
-}
-
 - (void) print
 {
     NSLog(@"==================================");
