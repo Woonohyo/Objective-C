54cbd9189464f59dbb4f1d3f51ec139caa8aa0be
Woonohyo <lineplus@LinePlus-ui-iMac.local>
2014-01-16 14:11:55 +0900

    prog 16.9 NSURL class

--- ObjectiveC2.0/main.m
+++ ObjectiveC2.0/main.m
@@ -15,41 +15,10 @@ int main(int argc, const char * argv[])
 {
     
     @autoreleasepool {
-        NSFileHandle        *inFile, *outFile;
-        NSData        *buffer;
+        NSURL *myURL = [NSURL URLWithString:@"http://www.naver.com"];
         
-        // open the fileA for reading
-        
-        inFile = [NSFileHandle fileHandleForReadingAtPath:@"/Users/lineplus/Documents/Objective-C/ObjectiveC2.0/fileA"];
-        
-        if (inFile == nil) {
-            NSLog(@"Open of fileA for reading failed");
-            return 1;
-        }
-        
-        // open the fileB for updating
-        
-        outFile = [NSFileHandle fileHandleForUpdatingAtPath:@"/Users/lineplus/Documents/Objective-C/ObjectiveC2.0/fileB"];
-        
-        if (outFile == nil) {
-            NSLog(@"Open of fileB for writing failed");
-            return 2;
-        }
-        
-        // Seek to the end of outFile
-        [outFile seekToEndOfFile];
-        
-        // read inFile and write its contents to outFile.
-        buffer = [inFile readDataToEndOfFile];
-        [outFile writeData: buffer];
-        
-        // Close the two files
-        [inFile closeFile];
-        [outFile closeFile];
-        
-        // verify its contents.
-        
-        NSLog(@"%@", [NSString stringWithContentsOfFile: @"/Users/lineplus/Documents/Objective-C/ObjectiveC2.0/fileB" encoding: NSUTF8StringEncoding error: NULL]);
+        NSString *myHomePage = [NSString stringWithContentsOfURL:myURL encoding:NSUTF8StringEncoding error: NULL];
+        NSLog(@"%@", myHomePage);
     }
     return 0;
 }
