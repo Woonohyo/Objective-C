1c9002ac83033a44ff940ae0797ec7eea7303f5c
Woonohyo <lineplus@LinePlus-ui-iMac.local>
2014-01-16 15:20:55 +0900

    prog 18.1 copy & mutable copy method

--- ObjectiveC2.0/main.m
+++ ObjectiveC2.0/main.m
@@ -15,10 +15,35 @@ int main(int argc, const char * argv[])
 {
     
     @autoreleasepool {
-        NSURL *myURL = [NSURL URLWithString:@"http://www.naver.com"];
+        NSMutableArray      *dataArray = [NSMutableArray arrayWithObjects:@"one", @"two", @"three", @"four", nil];
+        NSMutableArray      *dataArray2;
         
-        NSString *myHomePage = [NSString stringWithContentsOfURL:myURL encoding:NSUTF8StringEncoding error: NULL];
-        NSLog(@"%@", myHomePage);
+        // simple assignment
+        
+        dataArray2 = dataArray;
+        [dataArray2 removeObjectAtIndex: 0];
+        
+        NSLog(@"dataArray: ");
+        for ( NSString *elem in dataArray )
+            NSLog(@"    %@", elem);
+        
+        NSLog(@"dataArray2: ");
+        for ( NSString *elem in dataArray2 )
+            NSLog(@"    %@", elem);
+        
+        // try a copy, then remove the first elemtns from the copy
+        
+        dataArray2 = [dataArray mutableCopy];
+        [dataArray2 removeObjectAtIndex: 0];
+        
+        NSLog(@"dataArray: ");
+        for ( NSString *elem in dataArray )
+            NSLog(@"    %@", elem);
+        
+        NSLog(@"dataArray2: ");
+        for ( NSString *elem in dataArray2 )
+            NSLog(@"    %@", elem);
+
     }
     return 0;
 }
