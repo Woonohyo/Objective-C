988bf7b2ba43aab2736548da5402a6f3b78871db
Woonohyo <lineplus@LinePlus-ui-iMac.local>
2014-01-16 16:15:42 +0900

    Dictionary to plist - prog 19.1

--- ObjectiveC2.0/main.m
+++ ObjectiveC2.0/main.m
@@ -15,18 +15,17 @@ int main(int argc, const char * argv[])
 {
     
     @autoreleasepool {
-        Fraction *f1 = [[Fraction alloc] init];
-        Fraction *f2;
+        NSDictionary *glossary = @{
+                                   @"abstract class":
+                                       @"A class defined so other classes can inherit from it",
+                                   @"adopt":
+                                       @"To implement all the methods defined in a protocol",
+                                   @"archiving":
+                                       @"Storing an object for later use"
+                                   };
         
-        [f1 setTo: 2 over: 5];
-        
-        f2 = [f1 copy];
-        
-        [f2 setTo: 1 over: 3];
-        
-        [f1 print];
-        [f2 print];
-        
+        if ([glossary writeToFile: @"/Users/lineplus/Documents/Objective-C/ObjectiveC2.0/glossary" atomically: YES] == NO)
+            NSLog(@"Save to file failed");
     }
     return 0;
 }
